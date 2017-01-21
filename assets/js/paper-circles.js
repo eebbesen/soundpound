d7ebcc0a8f6985c6dcbe260c4f49a5bf14e90341
Eric Ebbesen <eebbesen.git@gmail.com>
2017-01-21 08:35:12 -0600

    More efficient

--- assets/js/paper-circles.js
+++ assets/js/paper-circles.js
@@ -2,8 +2,7 @@ var max = 1500;
 
 for (var y = 15; y < max; y += 30){
   for (var x = 15; x < max; x += 30) {
-    var myCircle = new Path.Circle(new Point(x, y), 10);
-    myCircle.fillColor = 'black';
+    new Path.Circle(new Point(x, y), 10).fillColor = 'orange';
   }
 }
   
