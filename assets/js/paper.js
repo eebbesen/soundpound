8ab51fae62a5f38ad6f959ef8e2fdf4e242f06b0
Eric Ebbesen <eebbesen.git@gmail.com>
2017-01-21 08:20:33 -0600

    First paper exercise: ball and line

---
+++ assets/js/paper.js
@@ -0,0 +1,12 @@
+
+// line
+var path = new Path();
+path.strokeColor = 'orange';
+var start = new Point(100, 100);
+path.moveTo(start);
+path.lineTo(start = [300, -50 ]);
+
+// circle
+var myCircle = new Path.Circle(new Point(100, 70), 50);
+myCircle.fillColor = 'black';
+  
