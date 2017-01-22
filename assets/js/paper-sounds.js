40f05ced472c4c02a90c7fd94f854f02609c8b3e
Eric Ebbesen <eebbesen.git@gmail.com>
2017-01-22 16:01:37 -0600

    starting with random points

--- assets/js/paper-sounds.js
+++ assets/js/paper-sounds.js
@@ -1,10 +1,32 @@
-var text = new PointText({
-  point: view.center,
-  content: 'Click here',
-  justification: 'center',
-  fontSize: 15
-});
 
 function onKeyDown(event){
-  text.content = event.key + ' was pressed down';
+  var p = getPoint();
+  getCircle(p);
+  showKey(event.key, p);
+}
+
+function getPoint(){
+  return new Point(getRandom(1500), getRandom(500));
+}
+
+function getCircle(point){
+  new Path.Circle(point, getRandom(60)).fillColor = 'orange';
+}
+
+function getRandom(seed){
+  if (!seed > 0){
+    seed = 900;
+  }
+
+  return Math.random() * seed;
+}
+
+function showKey(key, point){
+  // alert(point.x);
+  new PointText({
+    point: point,
+    content: key,
+    justification: 'center',
+    fontSize: 15
+  });
 }
